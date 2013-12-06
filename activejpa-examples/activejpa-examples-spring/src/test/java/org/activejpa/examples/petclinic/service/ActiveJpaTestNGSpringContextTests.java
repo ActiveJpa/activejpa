dc12f25c7126ab7f8edfbe6827cf4a2f6f0652c7
ganeshs <ganeshs@flipkart.com>
2013-12-06 09:28:35 +0530

    Fixed issue #9

--- activejpa-examples/activejpa-examples-spring/src/test/java/org/activejpa/examples/petclinic/service/ActiveJpaTestNGSpringContextTests.java
+++ activejpa-examples/activejpa-examples-spring/src/test/java/org/activejpa/examples/petclinic/service/ActiveJpaTestNGSpringContextTests.java
@@ -3,17 +3,17 @@
  */
 package org.activejpa.examples.petclinic.service;
 
-import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import javax.persistence.EntityManagerFactory;
 
+import org.activejpa.entity.testng.DomainClassObjectFactory;
 import org.activejpa.jpa.JPA;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.IObjectFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.ObjectFactory;
-import org.testng.internal.ObjectFactoryImpl;
 
 /**
  * @author ganeshs
@@ -23,12 +23,7 @@ public class ActiveJpaTestNGSpringContextTests extends AbstractTestNGSpringConte
 
 	@ObjectFactory
 	public IObjectFactory getObjectFactory(ITestContext context) throws Exception {
-		Class<?> clazz = Class.forName("org.activejpa.enhancer.ActiveJpaAgentLoader");
-		Method method = clazz.getMethod("instance");
-		Object loader = method.invoke(null);
-		method = clazz.getMethod("loadAgent");
-		method.invoke(loader);
-		return new ObjectFactoryImpl();
+		return new DomainClassObjectFactory(Arrays.asList("org.testng.", "org.xml"));
 	}
 	
 	@BeforeClass
